5ecc784c2a62e47291f0af245283c6a4dc7fbcd2
margo <DrozdMargo@i.ua>
2018-08-20 08:06:30 +0300

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <Hall :seats-data="seatsData"></Hall>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Hall from './components/Hall.vue'
+  import SeatsData from './data/seats.json'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      Hall
+    },
+    create: {},
+    data() {
+      return {
+        //simulate request to api to get data
+        seatsData: SeatsData,
+      }
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Hall.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div v-for="row in seatsData.rows" :key="row.id">
+            {{row.index + 1}} .
+            <seat-item :seats="row.seats" @checked-item="selectSeat"></seat-item>
+        </div>
+        {{selectedSeats}}
+    </div>
+</template>
+
+<script>
+  import SeatItem from './SeatItem.vue'
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      seatsData: Object
+    },
+    components: {
+      SeatItem,
+    },
+    data() {
+      return {
+        selectedSeats: [],
+      }
+    },
+    methods: {
+      selectSeat(seat) {
+        this.selectedSeats.push(seat.id)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+</style>
